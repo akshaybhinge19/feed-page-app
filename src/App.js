01465e9e2fb57a05f3215adc3e50ca3715bf711d
Akshay <akshaybhinge19@gmail.com>
2021-09-26 03:00:55 +0530

    done with functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {React,useState} from 'react';
+import Firstpage from './components/Firstpage';
+import Secondpage from './components/Secondpage';
+import Thirdpage from './components/Thirdpage';
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+// import { Route, Switch} from 'react-router-dom';
 function App() {
+  const [page, setPage] = useState(1);
+  function handleChange(event,value){
+      setPage(value);
+  }
+  const renderComponent=()=>{
+    if(page===1){
+      return <Firstpage/>
+    }
+    else if(page===2){
+      return <Secondpage/>
+    }
+    else {
+      return <Thirdpage/>
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-main">
+      <Typography align="left" variant='h3' component="div" color="neutral" sx={{fontFamily:'Raleway'}}>
+        Feed Flash
+      </Typography>
+      {renderComponent()}      
+      <Pagination count={3} align="center" color="secondary" size="large" page={page} onChange={handleChange}/>
     </div>
   );
 }
